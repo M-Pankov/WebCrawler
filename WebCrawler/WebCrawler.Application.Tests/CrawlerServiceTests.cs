2f51d210a1ba76f1e3153c90b835e9b667fa8570
MrSmoyk <npankov96@gmail.com>
2023-05-23 19:30:07 +0300

    minor changes

--- WebCrawler/WebCrawler.Application.Tests/CrawlerServiceTests.cs
+++ WebCrawler/WebCrawler.Application.Tests/CrawlerServiceTests.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebCrawler.Application.Tests
+namespace WebCrawler.Application.Tests;
+
+public class CrawlerServiceTests
 {
-    internal class CrawlerServiceTests
-    {
-    }
 }
